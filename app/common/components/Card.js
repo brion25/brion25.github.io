4e67bb3bbcd35e7114328e0bb7170612dd46a645
Carlos Gomez Briones <car_25_ixco@hotmail.com>
2016-03-15 23:16:32 -0600

    Adding styles to Home view

--- app/common/components/Card.js
+++ app/common/components/Card.js
@@ -6,8 +6,10 @@ import classNames from 'classnames';
 
 class Card extends Component{
   render(){
+    let cardStyle = this.props.cardStyle;
+
     return (
-      <div className={classNames('card', 'main-card')}>
+      <div className={classNames('card', cardStyle)}>
         <div className={classNames('card-header')}>
           <h2>{this.props.cardHeader}</h2>
         </div>
--- app/home/Home.js
+++ app/home/Home.js
@@ -10,7 +10,17 @@ class Home extends Component{
   constructor(props){
     super(props);
     this.state = {
-      profile : JSON.parse(window.localStorage.getItem('linkedinProfile'))
+      profile : JSON.parse(window.localStorage.getItem('linkedinProfile')),
+      options : [
+        {
+          name : 'Resume',
+          url : 'resume'
+        },
+        {
+          name : 'Github',
+          url : 'github'
+        }
+      ]
     }
   }
 
@@ -18,13 +28,26 @@ class Home extends Component{
     console.log(this.state);
     return (
       <div className={classNames('column','home')}>
-        <Card cardHeader={'Summary'}>
-          <div>
+        <Card cardHeader={'Summary'} cardStyle={'card-style-1'}>
+          <div className={classNames('row','center-items','wrap-content','center-content','profile')}>
             <img className={classNames('rounded','profile-image')} src={this.state.profile.pictureUrls.values[0]} />
-            <div>{this.state.profile.summary}</div>
+            <div className={classNames('container','column', 'profile-summary')}>
+              <h3 className={classNames('profile-header')}>{this.state.profile.formattedName}</h3>
+              <h4 className={classNames('profile-subheader')}>{this.state.profile.headline}</h4>
+              <div>{this.state.profile.summary}</div>
+            </div>
+          </div>
+          <div className={classNames('row','center-items','wrap-content','center-content')}>
+            {this.state.options.map(this.renderOptions.bind(this))}
           </div>
         </Card>
       </div>
+    );
+  }
+
+  renderOptions(option, i){
+    return (
+      <button key={'option-'+i} className={classNames('profile-option')}>{option.name}</button>
     );
   }
 }
