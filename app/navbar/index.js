5c48c17e8173dcc169d4e0348fb02f40fc1d1cb8
JC Gomez <car_25_ixco@hotmai.com>
2016-07-30 11:13:11 -0500

    Refactor to Navbar to wrap the menu options

--- app/navbar/index.js
+++ app/navbar/index.js
@@ -1,38 +1,27 @@
 import classnames from 'classnames';
 import * as types from './../actions';
 
+import menu from './menu';
+import searchMode from './search-mode';
+
 function Navbar(React){
+  const Menu = menu(React);
+  const SearchMode = searchMode(React);
+
   const Wrapper = (props) => {
     console.log(props);
-    const self = this;
 
     return (
       <div className="navbar">
         <div className={classnames("menu", {active : props.navbar.menuOpen})}>
-          This will be the Menu :D
+          <Menu />
         </div>
         <div className="row end-md end-xs navbar-actions">
           <div className="col-md-6 actions-wrapper col-xs-12">
             <div className={classnames("search-mode", {active : props.navbar.searchMode, 'menu-open' : props.navbar.menuOpen})}>
-              <div className="search-mode-actions">
-                <div onClick={openSearchMode} className="search-mode-search-text">
-                  <i className="fa fa-search search-icon" aria-hidden="true"></i>
-                  <div>
-                    <span>
-                      Search
-                    </span>
-                    <input type="text" placeholder="Search" />
-                  </div>
-                </div>
-                <div className="search-mode-close" onClick={closeSearchMode}>
-                  <i className="fa fa-times" aria-hidden="true"></i>
-                </div>
-              </div>
-              <div className="search-mode-content">
-                Comming Soon!
-              </div>
+              <SearchMode {...props} />
             </div>
-            <div className="menu-options">
+            <div className={classnames("menu-options",{'search-mode-active' : props.navbar.searchMode})}>
               <a className={classnames("menu-icon-open",{ active : !props.navbar.menuOpen })} onClick={openMenu}>
                 <i className="fa fa-th-large" aria-hidden="true"></i>
               </a>
@@ -51,14 +40,6 @@ function Navbar(React){
 
     function closeMenu(){
       props.dispatch(types.closeMenu());
-    }
-
-    function openSearchMode(){
-      props.dispatch(types.enableSearchMode());
-    }
-
-    function closeSearchMode(){
-      props.dispatch(types.disableSearchMode());
     }
   }
 
---
+++ app/navbar/menu.js
@@ -0,0 +1,13 @@
+function Menu (React){
+  const Wrapper = (props) => {
+    return (
+      <div>
+        This will be the Menu :D
+      </div>
+    );
+  }
+
+  return Wrapper;
+}
+
+export default Menu;
