50a17851091193a306cfe6a10ce992fb091a46ba
Jose Gomez <jose.gomez@unosquare.com>
2017-01-26 14:36:35 -0600

    Cleanning repo

--- app/index.js
+++
@@ -1,2 +0,0 @@
-import app from './app.scss';
-import './app.js';
--- versions/v1/app/home/index.js
+++
@@ -1,7 +0,0 @@
-import __view from "./view-home.jade";
-import __controller from "./controller-home.js";
-
-export default {
-  view : __view(),
-  controller : __controller
-}
