825873ef73ac5c69484d064716112badc566da2a
jose.gomez <jose.gomez@unosquare.com>
2017-06-16 11:12:23 -0500

    Adding meta tags for linkedin

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,18 @@ const getConfig = require('hjs-webpack')
 
 const config = getConfig({
   in: 'src/app.js',
-  out: '/',
+  out: 'dist/',
   clearBeforeBuild: true,
   html: function(context) {
     return {
       'index.html': context.defaultTemplate({
-        head: '<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">',
+        head: `
+            <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+            <meta property='og:title' content="Brion25 WebSite"/>
+            <meta property='og:image' content="https://raw.githubusercontent.com/brion25/brion25.github.io/master/src/common/assets/cara.png"/>
+            <meta property='og:description' content="I'm a JavaScript Ninja focused on developing the most proficient and scalable WebApps"/>
+            <meta property='og:url' content="http://brion25.github.io/" />
+            `,
         html: '<div id="root"></div><div id="particles-js"></div>'
       })
     }
