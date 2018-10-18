37213f5ca9688acb7fd715690e69cea80f800aa9
jose.gomez <jose.gomez@unosquare.com>
2018-10-18 18:55:53 -0500

    Using proper plugins setup

--- rollup.config.js
+++ rollup.config.js
@@ -3,27 +3,35 @@ import nodeResolve from 'rollup-plugin-node-resolve'
 import html from 'rollup-plugin-fill-html'
 import serve from 'rollup-plugin-serve'
 import string from 'rollup-plugin-string'
+import { argv } from 'yargs'
 import { resolve } from 'path'
+
+const plugins = [
+  nodeResolve({jsnext: true}),
+  commonjs(),
+  html({
+    template: resolve(__dirname, './src/index.html'),
+    filename: 'index.html'
+  }),
+  string({
+    include: '**/*.svg'
+  })
+]
+
+if (argv.watch) {
+  plugins.push(
+    serve({
+      open: true,
+      verbose: true,
+      contentBase: resolve(__dirname, './dist')
+    })
+  )
+}
 
 export default {
   entry: resolve(__dirname, './src/index.js'),
   dest: resolve(__dirname, './dist/bundle.js'),
   format: 'iife',
   sourceMap: true,
-  plugins: [
-    nodeResolve({jsnext: true}),
-    commonjs(),
-    html({
-      template: resolve(__dirname, './src/index.html'),
-      filename: 'index.html'
-    }),
-    serve({
-      open: true,
-      verbose: true,
-      contentBase: resolve(__dirname, './dist')
-    }),
-    string({
-      include: '**/*.svg'
-    })
-  ]
+  plugins: plugins
 }
