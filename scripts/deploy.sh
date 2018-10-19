901ca839d12a53e00e4bd244f3f97467f1df0f9e
jose.gomez <jose.gomez@unosquare.com>
2018-10-19 13:57:57 -0500

    pimping travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,9 +6,12 @@ git config --global user.email "clark.kent@dailyplanet.org"
 git config --global user.name "Superman"
 
 # deploy
+echo -e "\e[92m-----> Building the App <-----"
 npm run build
 cd dist
+echo -e "\e[92m-----> Deploying to Github <-----"
 git init
 git add .
 git commit -m "Deploy to Github Pages"
-git push --force "https://${githubToken}@github.com/brion25/brion25.github.io.git" master
+git push --force "https://${GITHUB_TOKEN}@github.com/brion25/brion25.github.io.git" master
+echo -e "\e[92m-----> Code deployed to Github <-----"
