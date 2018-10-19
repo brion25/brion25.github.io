f63b1009791a7826a349657314c03979ad558a02
jose.gomez <jose.gomez@unosquare.com>
2018-10-19 13:38:26 -0500

    debug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,10 @@ git config --global user.email "clark.kent@dailyplanet.org"
 git config --global user.name "Superman"
 
 # deploy
+ls
 cd dist
+ls
 git init
 git add .
 git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${githubToken}@$github.com/brion25/brion25.github.io.git" master
+git push --force "https://${githubToken}@$github.com/brion25/brion25.github.io.git" master
